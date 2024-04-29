8ddbecd3bb35e4e2dc3458a5df1cc242cb60e465
kajing1077 <kajing1077@gmail.com>
2024-04-29 12:28:40 +0900

    feat: Add React Router and update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-function App() {
-  return (
-    <div>
-      <h1>React App</h1>
-    </div>
-  );
-}
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './pages/Login';
+import Reset from './pages/Reset';
+import Join from './pages/Join';
+import Layout from './components/common/layout/Layout';
+import Main from './pages/Main';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+  },
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/reset',
+        element: <Reset />,
+      },
+      {
+        path: '/join',
+        element: <Join />,
+      },
+    ],
+  },
+]);
+
+const App: React.FC = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
