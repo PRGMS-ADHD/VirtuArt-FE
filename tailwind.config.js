2d680f84613fe4774e3085ee612aac3d14849b1d
kajing1077 <kajing1077@gmail.com>
2024-04-29 12:40:10 +0900

    chore: Add corePlugins preflight and extend theme in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  corePlugins: {
+    preflight: true,
+  },
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'noto-sans-kr': ['"Noto Sans KR"', 'sans-serif'],
+        helvetica: ['Helvetica', 'sans-serif'],
+      },
+      colors: {
+        customGray: '#444',
+        customGray2: '#666',
+        customGray3: '#777',
+        customGray4: '#BBB',
+        inputBg: 'rgba(221, 221, 221, 0.2)',
+      },
+      flexShrink: {
+        10: '10',
+      },
+    },
   },
   plugins: [],
 };
