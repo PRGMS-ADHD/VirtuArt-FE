84a4bdacb38e7fb20fbceeac56f5f1200f7404a8
kajing1077 <kajing1077@gmail.com>
2024-04-29 12:36:04 +0900

    feat: Add Button component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,9 +1,21 @@
-const MyPage = () => {
+import { FC } from 'react';
+
+interface ButtonProps {
+  text: string;
+  onClick?: () => void;
+  type: 'button' | 'submit' | 'reset';
+}
+
+const Button: FC<ButtonProps> = ({ text, type, onClick }) => {
   return (
-    <div>
-      <h1>안녕하세요</h1>
-    </div>
+    <button
+      type={type}
+      className="h-[50px] w-[400px] flex-shrink-0 rounded-md bg-black font-helvetica text-base font-bold leading-normal text-white"
+      onClick={onClick}
+    >
+      {text}
+    </button>
   );
 };
 
-export default MyPage;
+export default Button;
