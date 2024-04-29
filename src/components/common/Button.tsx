bc0ce905f5234220cee3b9eb26f0c042aa63a8b3
kajing1077 <kajing1077@gmail.com>
2024-04-29 12:37:20 +0900

    docs: Add comments to the code

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 const Button: FC<ButtonProps> = ({ text, type, onClick }) => {
   return (
     <button
-      type={type}
+      type={type} // TODO 수정필요
       className="h-[50px] w-[400px] flex-shrink-0 rounded-md bg-black font-helvetica text-base font-bold leading-normal text-white"
       onClick={onClick}
     >
