0733b1b4cc95a7fc92a1eb9f0c74f9925854859d
kajing1077 <kajing1077@gmail.com>
2024-04-29 12:40:31 +0900

    chore: Add tailwindcss and autoprefixer plugins to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
 });
