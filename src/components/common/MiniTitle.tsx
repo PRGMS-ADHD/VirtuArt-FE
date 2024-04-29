cedbf9211ab89d0a149fb147154d82653d9e362c
kajing1077 <kajing1077@gmail.com>
2024-04-29 15:10:16 +0900

    #2 refactor: Refactor MiniTitle component to use function syntax instead of FC in MiniTitle.tsx

--- src/components/common/MiniTitle.tsx
+++ src/components/common/MiniTitle.tsx
@@ -1,17 +1,15 @@
-import { FC } from 'react';
-
 interface TitleProps {
   text: string;
   boldText: string;
 }
 
-const Title: FC<TitleProps> = ({ text, boldText }) => {
+function Title({ text, boldText }: TitleProps) {
   return (
     <div className="font-noto-sans-kr text-base leading-normal text-black">
       <span className="font-light">{text}</span>
       <span className="font-bold">{boldText}</span>
     </div>
   );
-};
+}
 
 export default Title;
