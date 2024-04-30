7f53a382a6d4083863ca55355419cfe06297289a
kajing1077 <kajing1077@gmail.com>
2024-04-30 14:34:26 +0900

    충돌 테스트 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,16 @@
-import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Login from './pages/Login';
-import Reset from './pages/Reset';
-import Join from './pages/Join';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './components/common/Layout';
-import Main from './pages/Main';
 import Gallery from './pages/Gallery';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Main />,
-  },
-  {
-    path: '/gallery',
     element: (
       <Layout>
         <Gallery />
       </Layout>
     ),
-  },
-  {
-    element: <Layout />,
-    children: [
-      {
-        path: '/login',
-        element: <Login />,
-      },
-      {
-        path: '/reset',
-        element: <Reset />,
-      },
-      {
-        path: '/signup',
-        element: <Join />,
-      },
-    ],
+    children: [{ path: '/', element: <Gallery /> }],
   },
 ]);
 
