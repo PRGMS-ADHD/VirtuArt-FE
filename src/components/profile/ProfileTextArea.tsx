e161b2f65a3d704f62964c786503266456c5d956
kajing1077 <kajing1077@gmail.com>
2024-04-30 12:51:31 +0900

    #4 style: refactor padding in TextArea

--- src/components/profile/ProfileTextArea.tsx
+++ src/components/profile/ProfileTextArea.tsx
@@ -1,6 +1,6 @@
 function ProfileTextArea() {
   return (
-    <textarea className="h-[125px] w-10/12 flex-shrink-0 resize-none rounded border border-gray-300 bg-white p-6 shadow-md" />
+    <textarea className="h-[125px] w-10/12 flex-shrink-0 resize-none rounded border border-gray-300 bg-white p-4 shadow-md" />
   );
 }
 
