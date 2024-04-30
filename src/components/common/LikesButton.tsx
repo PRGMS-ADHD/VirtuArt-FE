ef5e366ed0c8d25abc2becb293a1d3ee03953867
kajing1077 <kajing1077@gmail.com>
2024-04-30 11:27:49 +0900

    #1 refactor: Refactor LikesButton and SettingsButton components

--- src/components/common/LikesButton.tsx
+++ src/components/common/LikesButton.tsx
@@ -1,22 +1,43 @@
+// import React from 'react';
+// import { HeartIcon } from '@heroicons/react/20/solid';
+//
+// interface SettingsButtonProps {
+//   onClick?: () => void;
+// }
+//
+// const LikesButton: React.FC<SettingsButtonProps> = ({ onClick }) => {
+//   return (
+//     <button
+//       type="button"
+//       className="relative flex h-[30px] w-[100px] items-center justify-around shadow-md"
+//       onClick={onClick}
+//     >
+//       <div className="absolute left-0 top-0 h-[30px] w-[100px] bg-black" />
+//       <HeartIcon className="z-10 h-5 w-5 text-white" />
+//       <div className="z-10 ml-1 font-['Helvetica'] text-xs font-normal text-white">
+//         1234 K
+//       </div>
+//     </button>
+//   );
+// };
+//
+// export default LikesButton;
 import React from 'react';
 import { HeartIcon } from '@heroicons/react/20/solid';
 
-interface SettingsButtonProps {
+interface LikesButtonProps {
   onClick?: () => void;
 }
 
-const LikesButton: React.FC<SettingsButtonProps> = ({ onClick }) => {
+const LikesButton: React.FC<LikesButtonProps> = ({ onClick }) => {
   return (
     <button
       type="button"
-      className="relative flex h-[30px] w-[100px] items-center justify-around shadow-md"
+      className="flex h-[30px] w-[100px] items-center justify-around bg-black text-white shadow-md"
       onClick={onClick}
     >
-      <div className="absolute left-0 top-0 h-[30px] w-[100px] bg-black" />
-      <HeartIcon className="z-10 h-5 w-5 text-white" />
-      <div className="z-10 ml-1 font-['Helvetica'] text-xs font-normal text-white">
-        1234 K
-      </div>
+      <HeartIcon className="mr-2 h-5 w-5" />
+      <span className="font-['Helvetica'] text-xs font-normal">1234 K</span>
     </button>
   );
 };
--- src/components/common/SettingsButton.tsx
+++ src/components/common/SettingsButton.tsx
@@ -1,3 +1,27 @@
+// import React from 'react';
+// import { Cog6ToothIcon } from '@heroicons/react/20/solid';
+//
+// interface SettingsButtonProps {
+//   onClick?: () => void;
+// }
+//
+// const SettingsButton: React.FC<SettingsButtonProps> = ({ onClick }) => {
+//   return (
+//     <button
+//       type="button"
+//       className="relative flex h-[30px] w-[100px] items-center justify-center shadow-md"
+//       onClick={onClick}
+//     >
+//       <div className="absolute left-0 top-0 h-[30px] w-[100px] bg-black" />
+//       <Cog6ToothIcon className="z-10 h-4 w-4 text-white" />
+//       <div className="z-10 ml-1 font-['Helvetica'] text-xs font-normal text-white">
+//         SETTINGS
+//       </div>
+//     </button>
+//   );
+// };
+//
+// export default SettingsButton;
 import React from 'react';
 import { Cog6ToothIcon } from '@heroicons/react/20/solid';
 
@@ -9,14 +33,11 @@ const SettingsButton: React.FC<SettingsButtonProps> = ({ onClick }) => {
   return (
     <button
       type="button"
-      className="relative flex h-[30px] w-[100px] items-center justify-center shadow-md"
+      className="flex h-[30px] w-[100px] items-center justify-center bg-black text-white shadow-md"
       onClick={onClick}
     >
-      <div className="absolute left-0 top-0 h-[30px] w-[100px] bg-black" />
-      <Cog6ToothIcon className="z-10 h-4 w-4 text-white" />
-      <div className="z-10 ml-1 font-['Helvetica'] text-xs font-normal text-white">
-        SETTINGS
-      </div>
+      <Cog6ToothIcon className="mr-1 h-5 w-5" />
+      <span className="font-['Helvetica'] text-xs font-normal">SETTINGS</span>
     </button>
   );
 };
