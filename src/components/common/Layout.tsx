6efc75265244e46a22b233188c34e109111b8e8c
kajing1077 <kajing1077@gmail.com>
2024-04-30 16:08:20 +0900

    #1 refactor: Refactor Layout component to center children vertically

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,3 +1,22 @@
+// import React from 'react';
+// import Header from './Header';
+// import Footer from './Footer';
+//
+// interface LayoutProps {
+//   children: React.ReactNode;
+// }
+//
+// const Layout = ({ children }: LayoutProps) => {
+//   return (
+//     <div className="relative m-0 flex h-full max-w-[1920px] flex-col items-center justify-center p-0">
+//       <Header />
+//       {children}
+//       <Footer />
+//     </div>
+//   );
+// };
+//
+// export default Layout;
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
@@ -8,9 +27,11 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="relative m-0 flex h-full max-w-[1920px] flex-col items-center justify-center p-0">
+    <div className="flex flex-col min-h-screen">
       <Header />
-      {children}
+      <div className="flex flex-grow justify-center items-center">
+        {children}
+      </div>
       <Footer />
     </div>
   );
