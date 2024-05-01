9fdf2e65e8296e44dd05a81dece87b9fa76243da
kajing1077 <kajing1077@gmail.com>
2024-05-01 16:14:43 +0900

    #13 chore: Add new font and colors to tailwind.config.js
    - borderWidth, width: MyPage에서 사용, 수정 필요

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,15 @@ export default {
       fontFamily: {
         'noto-sans-kr': ['"Noto Sans KR"', 'sans-serif'],
         helvetica: ['Helvetica', 'sans-serif'],
+        helveticaNeue: ['Helvetica Neue', 'sans-serif'],
       },
       colors: {
         customGray: '#444',
         customGray2: '#666',
         customGray3: '#777',
         customGray4: '#BBB',
+        customGray5: '#EEE',
+        customGray6: '#DDD',
         inputBg: 'rgba(221, 221, 221, 0.2)',
       },
       flexShrink: {
@@ -23,6 +26,12 @@ export default {
       boxShadow: {
         'custom-dark':
           '0 4px 6px 0 rgba(0, 0, 0, 0.4), 0 5px 15px 0 rgba(0, 0, 0, 0.1)',
+      },
+      borderWidth: {
+        1: '1px',
+      },
+      width: {
+        1540: '1540px',
       },
     },
   },
