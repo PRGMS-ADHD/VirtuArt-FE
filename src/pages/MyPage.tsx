c8cfabb153f1195c3332ea0b17ffbad295119fd1
kajing1077 <kajing1077@gmail.com>
2024-05-02 16:36:52 +0900

    #13 refactor: Refactor MyPage into smaller components and Convert some inline styles to Tailwind CSS

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -2,16 +2,19 @@ import React, { useState } from 'react';
 import image from '../assets/image1.jpeg';
 import UserProfileCard from '../components/profile/UserProfileCard';
 import UserArtPieceLikes from '../data/userArtPieceLike';
-import LoadMoreButton from '../components/common/LoadMoreButton';
+import LikedSection from '../components/myPage/LikedSection';
+import { ArtPieceLike } from '../models/user.model';
 
 const MyPage: React.FC = () => {
   const [visibleImages, setVisibleImages] = useState(8);
   const [visibleArtists, setVisibleArtists] = useState(8);
-  const userImages = UserArtPieceLikes.filter((like) => like.userId === 1);
+  const userImages: ArtPieceLike[] = UserArtPieceLikes.filter(
+    (like) => like.userId === 1,
+  );
 
   const handleLoadMore = () => {
     if (userImages.length <= visibleImages) {
-      setVisibleImages(8); // Reset to initial value
+      setVisibleImages(8);
     } else {
       setVisibleImages(visibleImages + 8);
     }
@@ -39,78 +42,21 @@ const MyPage: React.FC = () => {
         <UserProfileCard />
       </div>
       <div>
-        <div className="bg-customGray5 my-6 flex flex-col items-center justify-center">
-          <div className="border-b-1 border-customGray6 mx-auto w-5/6">
-            <div className="px-8 pt-8">
-              <p className="font-helveticaNeue mb-1 ml-1">LIKED WORKS</p>
-              <div
-                style={{
-                  display: 'grid',
-                  gridTemplateColumns: 'repeat(4, 1fr)',
-                  gap: '1rem',
-                }}
-              >
-                {userImages.slice(0, visibleImages).map((like) => (
-                  <img
-                    key={like.id}
-                    src={like.imageUrl}
-                    alt={`Art Piece ${like.artPieceId}`}
-                    style={{
-                      width: '376px',
-                      height: '211px',
-                      objectFit: 'cover',
-                    }}
-                  />
-                ))}
-              </div>
-            </div>
-            <LoadMoreButton
-              onClick={handleLoadMore}
-              style={{
-                position: 'relative',
-                transform: 'translateY(50%)',
-                margin: 'auto',
-              }}
-              isExpanded={isAllImagesVisible}
-            />
-          </div>
-        </div>
-        <div className="my-6 flex flex-col items-center justify-center">
-          <div className="border-b-1 border-customGray6 mx-auto w-5/6">
-            <div className="px-8 pt-8">
-              <p className="font-helveticaNeue mb-1 ml-1">LIKED ARTISTS</p>
-              <div
-                style={{
-                  display: 'grid',
-                  gridTemplateColumns: 'repeat(4, 1fr)',
-                  gap: '1rem',
-                }}
-              >
-                {userImages.slice(0, visibleArtists).map((like) => (
-                  <img
-                    key={like.id}
-                    src={like.imageUrl}
-                    alt={`Art Piece ${like.artPieceId}`}
-                    style={{
-                      width: '376px',
-                      height: '211px',
-                      objectFit: 'cover',
-                    }}
-                  />
-                ))}
-              </div>
-            </div>
-            <LoadMoreButton
-              onClick={handleLoadMoreArtists}
-              style={{
-                position: 'relative',
-                transform: 'translateY(50%)',
-                margin: 'auto',
-              }}
-              isExpanded={isAllArtistsVisible}
-            />
-          </div>
-        </div>
+        <LikedSection
+          title="LIKED WORKS"
+          userImages={userImages}
+          visibleCount={visibleImages}
+          handleLoadMore={handleLoadMore}
+          isExpanded={isAllImagesVisible}
+          backgroundColor="bg-customGray5"
+        />
+        <LikedSection
+          title="LIKED ARTISTS"
+          userImages={userImages}
+          visibleCount={visibleArtists}
+          handleLoadMore={handleLoadMoreArtists}
+          isExpanded={isAllArtistsVisible}
+        />
       </div>
     </div>
   );
