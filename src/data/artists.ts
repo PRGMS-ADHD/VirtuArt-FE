c967a00bc9c3e70c61fc366340476f5ec9f1e366
희호 <heraok0153@gmail.com>
2024-05-07 19:02:52 +0900

    #14 Add dummy data for artists

---
+++ src/data/artists.ts
@@ -0,0 +1,129 @@
+interface Artist {
+  id: number;
+  name: string;
+  imageUrl: string;
+  category: string;
+}
+
+export const Artists: Artist[] = [
+  {
+    id: 1,
+    name: '빈센트 반 고흐',
+    imageUrl: '/Western/Artist1.png',
+    category: '서양화',
+  },
+  {
+    id: 2,
+    name: '폴 고갱',
+    imageUrl: '/Western/Artist2.png',
+    category: '서양화',
+  },
+  {
+    id: 3,
+    name: '클로드 모네',
+    imageUrl: '/Western/Artist3.png',
+    category: '서양화',
+  },
+  {
+    id: 4,
+    name: '레오나르도 다빈치',
+    imageUrl: '/Western/Artist4.png',
+    category: '서양화',
+  },
+  {
+    id: 5,
+    name: '파블로 피카소',
+    imageUrl: '/Western/Artist5.png',
+    category: '서양화',
+  },
+  {
+    id: 6,
+    name: '살바도르 달리',
+    imageUrl: '/Western/Artist6.png',
+    category: '서양화',
+  },
+  {
+    id: 7,
+    name: '카스퍼 다비드 프리드리히',
+    imageUrl: '/Western/Artist1.png',
+    category: '동양화',
+  },
+  {
+    id: 8,
+    name: '에드바르 뭉크',
+    imageUrl: '/Western/Artist2.png',
+    category: '미디어 아트',
+  },
+  {
+    id: 9,
+    name: '야요이 쿠사마',
+    imageUrl: '/Western/Artist3.png',
+    category: '현대 미술',
+  },
+  {
+    id: 10,
+    name: '앙리 마티스',
+    imageUrl: '/Western/Artist4.png',
+    category: '팝 아트',
+  },
+  {
+    id: 11,
+    name: '게오르기아 오키프',
+    imageUrl: '/Western/Artist5.png',
+    category: '서양화',
+  },
+  {
+    id: 12,
+    name: '마르크 샤갈',
+    imageUrl: '/Western/Artist1.png',
+    category: '서양화',
+  },
+  {
+    id: 13,
+    name: '장 미셸 바스키아',
+    imageUrl: '/Western/Artist2.png',
+    category: '서양화',
+  },
+  {
+    id: 14,
+    name: '조지아 오키프',
+    imageUrl: '/Western/Artist3.png',
+    category: '서양화',
+  },
+  {
+    id: 15,
+    name: '프리다 칼로',
+    imageUrl: '/Western/Artist4.png',
+    category: '서양화',
+  },
+  {
+    id: 16,
+    name: '앤디 워홀',
+    imageUrl: '/Western/Artist5.png',
+    category: '팝 아트',
+  },
+  {
+    id: 17,
+    name: '클로드 모네',
+    imageUrl: '/Western/Artist1.png',
+    category: '현대 미술',
+  },
+  {
+    id: 18,
+    name: '에드가 드가',
+    imageUrl: '/Western/Artist2.png',
+    category: '동양화',
+  },
+  {
+    id: 19,
+    name: '토마스 콜',
+    imageUrl: '/Western/Artist3.png',
+    category: '서양화',
+  },
+  {
+    id: 20,
+    name: '윌리엄 터너',
+    imageUrl: '/Western/Artist4.png',
+    category: '미디어 아트',
+  },
+];
