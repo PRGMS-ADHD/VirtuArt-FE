bf9078fc9e6f759e05170b54fb9b8793c677644c
kajing1077 <kajing1077@gmail.com>
2024-05-08 17:49:26 +0900

    #4 feat: Add ArtistProfileCard and UserProfileCard components

--- src/components/profile/ArtistProfileCard.tsx
+++ src/components/profile/ArtistProfileCard.tsx
@@ -1,8 +1,8 @@
 import React, { ReactNode } from 'react';
+import { Artist } from '@/models/artist.model';
 import ProfileLinks from './ProfileLinks';
 import ProfileTextArea from './ProfileTextArea';
 import ProfilePicture from './ProfilePicture';
-import logo from '../../assets/test1.png';
 import image from '../../assets/image1.jpeg';
 
 // export default ProfileCard;
@@ -14,11 +14,15 @@ import image from '../../assets/image1.jpeg';
 //   onSettingsClick?: () => void;
 // }
 
-interface UserProfileCardProps {
+interface ArtistProfileCardProps {
   children?: ReactNode;
+  artist: Artist;
 }
 
-const UserProfileCard: React.FC<UserProfileCardProps> = ({ children }) => {
+const ArtistProfile: React.FC<ArtistProfileCardProps> = ({
+  children,
+  artist,
+}) => {
   return (
     <div className="flex flex-col items-center justify-center">
       <img
@@ -28,27 +32,27 @@ const UserProfileCard: React.FC<UserProfileCardProps> = ({ children }) => {
       />
       <div className="relative flex h-[256px] w-[1196px] flex-col justify-end">
         <ProfilePicture
-          src={logo}
+          src={artist.profile_image}
           className="absolute left-[-54px] top-[-54px]"
         />
         <div className="mt-6 flex items-center justify-between">
           <div className="flex items-center">
             <p className="ml-[80px] font-noto-sans-kr text-3xl font-normal leading-normal text-black">
-              정센트 반 희호
+              {artist.name}
             </p>
-            <p className="font-noto-sans-kr text-sm font-normal leading-normal text-black">
-              &nbsp;(Jeongcent Van Heeho)
+            <p className="font-noto-sans-kr text-base font-normal leading-normal text-black">
+              &nbsp;({artist.e_name})
             </p>
           </div>
           <div className="flex gap-x-4">{children}</div>
         </div>
         <ProfileLinks />
         <div className="flex flex-1 items-center justify-center">
-          <ProfileTextArea />
+          <ProfileTextArea text={artist.intro} />
         </div>
       </div>
     </div>
   );
 };
 
-export default UserProfileCard;
+export default ArtistProfile;
