ef52d4cfb7484c94e59e97a06cec3cf1ef32153a
kajing1077 <kajing1077@gmail.com>
2024-05-08 18:17:26 +0900

    chore: Add vite-tsconfig-paths plugin to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
