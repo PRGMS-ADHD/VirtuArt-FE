1508b76cb81be8fc2b564b027a4b1ed7f7ed7eff
kajing1077 <kajing1077@gmail.com>
2024-05-08 17:17:14 +0900

    #1 feat: Improve transition animation for LoadMoreButton

--- src/components/common/LoadMoreButton.tsx
+++ src/components/common/LoadMoreButton.tsx
@@ -21,7 +21,7 @@ const LoadMoreButton: React.FC<LoadMoreButtonProps> = ({
       className="relative m-auto flex h-[36px] w-[40px] translate-y-1/2 items-center justify-center rounded-[50%] border border-customGray6 bg-white duration-300"
     >
       <ChevronDoubleDownIcon
-        className={`h-5 w-5 ${isExpanded ? 'rotate-180' : ''}`}
+        className={`h-5 w-5 transition-transform duration-700 ${isExpanded ? 'rotate-180' : ''}`}
         fill="#777"
         strokeWidth={1}
       />
