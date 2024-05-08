acc507e5d1aa22d82259b2aa777050a4181d09ed
kajing1077 <kajing1077@gmail.com>
2024-05-08 18:27:07 +0900

    #15 feat: modify ArtPieceDetail component for verification

--- src/components/galleryDetail/ArtPieceDetail.tsx
+++ src/components/galleryDetail/ArtPieceDetail.tsx
@@ -1,6 +1,21 @@
 import { useState } from 'react';
-import { ArtPieces } from '../../data/artPieces';
+import { ArtPieces } from '@/data/artPieces';
 import { useParams } from 'react-router-dom';
+import LikesCountButton from '@/components/common/LikesCountButton';
+import OtherWorks from '@/components/artist/OtherWorks';
+import Collectors from '@/components/collectors/Collectors';
+import HorizonProfileCard from '@/components/profile/HorizonProfileCard';
+import LikesButton from '@/components/common/LikesButton';
+import React from 'react';
+
+function convertNewlineToBreak(text: string) {
+  return text.split('\n').map((line) => (
+    <React.Fragment>
+      {line}
+      <br />
+    </React.Fragment>
+  ));
+}
 
 const ArtPieceDetail = () => {
   const { id } = useParams<{ id: string }>();
@@ -20,8 +35,8 @@ const ArtPieceDetail = () => {
   };
 
   return (
-    <div className="flex flex-col p-10">
-      <div className="flex flex-row justify-between">
+    <div className="flex-1 justify-center pb-8">
+      <div className="flex flex-row justify-between p-10 px-36 pb-4">
         <div className="flex flex-col">
           <h1 className="text-xl" style={{ fontFamily: 'noto-sans KR' }}>
             {artPiece.title}
@@ -33,46 +48,66 @@ const ArtPieceDetail = () => {
             {artPiece.engTitle}
           </p>
         </div>
-        <p className="">좋아요 갯수</p>
+        <div className="">
+          <LikesCountButton />
+        </div>
       </div>
 
-      <div className="my-5 flex flex-row justify-center">
-        <div className="border-1 mx-8 w-1/2 border py-20 drop-shadow-lg">
-          <img src={artPiece.imageUrl} alt={artPiece.title} />
+      <div className="my-5 flex flex-col items-start justify-evenly gap-5 pb-6 xl:flex-row">
+        <div className="shadow-custom-shadow relative mx-0 flex h-[38.5rem] w-full items-center justify-center xl:w-[37.5rem]">
+          <img
+            src={artPiece.imageUrl}
+            alt={artPiece.title}
+            style={{
+              maxHeight: '100%',
+              maxWidth: '100%',
+            }}
+          />
         </div>
 
-        <div className="flex w-1/2 flex-col">
-          <div className="flex items-center justify-center">
-            <button
-              className={`px-4 py-2 focus:outline-none ${
-                selectedTab === 'description'
-                  ? 'border-b-2 border-black font-semibold'
-                  : 'border-b border-gray-400'
-              }`}
-              onClick={() => handleTabClick('description')}
-            >
-              DESCRIPTION
-            </button>
-            <button
-              className={`px-4 py-2 focus:outline-none ${
-                selectedTab === 'info'
-                  ? 'border-b-2 border-black font-semibold'
-                  : 'border-b border-gray-400'
-              }`}
-              onClick={() => handleTabClick('info')}
-            >
-              INFO
-            </button>
+        <div className="flex w-full flex-col items-center xl:w-[37.5rem]">
+          <div className="flex justify-center">
+            <div className="z-10 -mb-1 flex w-full justify-between border-gray-400 px-8">
+              <button
+                type="button"
+                className={`flex-1 px-4 pb-2 focus:outline-none ${
+                  selectedTab === 'description'
+                    ? 'border-b-4 border-black font-semibold'
+                    : ''
+                }`}
+                onClick={() => handleTabClick('description')}
+              >
+                DESCRIPTION
+              </button>
+              <button
+                type="button"
+                className={`flex-1 px-4 pb-2 focus:outline-none ${
+                  selectedTab === 'info'
+                    ? 'border-b-4 border-black font-semibold'
+                    : ''
+                }`}
+                onClick={() => handleTabClick('info')}
+              >
+                INFO
+              </button>
+            </div>
           </div>
-          <div className="border-1 mx-8  overflow-y-auto p-5">
+          <div className="mx-8 max-h-[36rem] overflow-y-auto border-1 border-t-4 p-4 xl:w-[37.5rem]">
             {selectedTab === 'description' ? (
-              <p>{artPiece.description}</p>
+              <p>{convertNewlineToBreak(artPiece.description)}</p>
             ) : (
-              <p>{artPiece.info}</p>
+              <p>{convertNewlineToBreak(artPiece.info)}</p>
             )}
           </div>
         </div>
       </div>
+      <div>
+        <HorizonProfileCard>
+          <LikesButton />
+        </HorizonProfileCard>
+      </div>
+      <OtherWorks />
+      <Collectors />
     </div>
   );
 };
