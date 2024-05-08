df93e76bb100523a06608c28b43039f9f13b76e9
kajing1077 <kajing1077@gmail.com>
2024-05-08 18:17:13 +0900

    #22 feat: Add Artist model

---
+++ src/models/artist.model.ts
@@ -0,0 +1,13 @@
+export interface Artist {
+  _id: string;
+  name: string;
+  e_name: string;
+  likes: number;
+  instagram: string;
+  location: string;
+  intro: string;
+  collectors: string[];
+  cover_image: string;
+  profile_image: string;
+  __v: number;
+}
