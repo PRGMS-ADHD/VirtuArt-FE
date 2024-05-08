e74ba2290964514cfe2ab7be96954c2b8c442e04
kajing1077 <kajing1077@gmail.com>
2024-05-08 17:50:09 +0900

    chore: Update eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -32,12 +32,19 @@ module.exports = {
     'jsx-a11y',
     'import',
   ],
+  'settings': {
+    'import/resolver': {
+      'typescript': {
+        'alwaysTryTypes': true,
+      },
+    },
+  },
   rules: {
     'react/react-in-jsx-scope': 'off',
     'prettier/prettier': ['error'],
-    'no-param-reassign': ['error', { props: false }],
-    'import/no-extraneous-dependencies': ['error', { devDependencies: false }],
+    'no-param-reassign': ['error', {props: false}],
+    'import/no-extraneous-dependencies': ['error', {devDependencies: false}],
     'react/function-component-definition': 'off',
-    "react/require-default-props": "off"
+    'react/require-default-props': 'off',
   },
 };
