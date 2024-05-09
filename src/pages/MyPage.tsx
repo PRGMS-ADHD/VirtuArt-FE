9240d4a490727ba7b99ac32705ad99a64ada79b8
kajing1077 <kajing1077@gmail.com>
2024-05-09 16:24:55 +0900

    #13 refactor: Refactor MyPage component

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,62 +1,22 @@
-import React, { useState } from 'react';
-import image from '../assets/image1.jpeg';
+import React from 'react';
 import UserProfileCard from '../components/profile/UserProfileCard';
-import UserArtPieceLikes from '../data/userArtPieceLike';
-import LikedSection from '../components/myPage/LikedSection';
-import { ArtPieceLike } from '../models/user.model';
+import LikedWorks from '../components/myPage/LikedWorks';
+import LikedArtists from '../components/myPage/LikedArtists';
+import LikesCountButton from '../components/common/LikesCountButton';
+import SettingsButton from '../components/common/SettingsButton';
 
 const MyPage: React.FC = () => {
-  const [visibleImages, setVisibleImages] = useState(8);
-  const [visibleArtists, setVisibleArtists] = useState(8);
-  const userImages: ArtPieceLike[] = UserArtPieceLikes.filter(
-    (like) => like.userId === 1,
-  );
-
-  const handleLoadMore = () => {
-    if (userImages.length <= visibleImages) {
-      setVisibleImages(8);
-    } else {
-      setVisibleImages(visibleImages + 8);
-    }
-  };
-
-  const handleLoadMoreArtists = () => {
-    if (userImages.length <= visibleArtists) {
-      setVisibleArtists(8);
-    } else {
-      setVisibleArtists(visibleArtists + 8);
-    }
-  };
-
-  const isAllImagesVisible = userImages.length <= visibleImages;
-  const isAllArtistsVisible = userImages.length <= visibleArtists;
-
   return (
     <div className="mb-6">
-      <img
-        src={image}
-        alt="image1.png"
-        className="h-[330px] w-[1920px] object-cover opacity-60"
-      />
       <div className="flex justify-center">
-        <UserProfileCard />
+        <UserProfileCard>
+          <LikesCountButton />
+          <SettingsButton />
+        </UserProfileCard>
       </div>
       <div>
-        <LikedSection
-          title="LIKED WORKS"
-          userImages={userImages}
-          visibleCount={visibleImages}
-          handleLoadMore={handleLoadMore}
-          isExpanded={isAllImagesVisible}
-          backgroundColor="bg-customGray5"
-        />
-        <LikedSection
-          title="LIKED ARTISTS"
-          userImages={userImages}
-          visibleCount={visibleArtists}
-          handleLoadMore={handleLoadMoreArtists}
-          isExpanded={isAllArtistsVisible}
-        />
+        <LikedWorks />
+        <LikedArtists />
       </div>
     </div>
   );
