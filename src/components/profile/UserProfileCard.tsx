512ad054087639076c94f8af262c23c8e066846c
kajing1077 <kajing1077@gmail.com>
2024-05-09 16:24:37 +0900

    #13 feat: Update UserProfileCard component with new image and layout

--- src/components/profile/UserProfileCard.tsx
+++ src/components/profile/UserProfileCard.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import ProfileLinks from './ProfileLinks';
 import ProfileTextArea from './ProfileTextArea';
-import LikesButton from '../common/LikesButton';
-import SettingsButton from '../common/SettingsButton';
 import ProfilePicture from './ProfilePicture';
 import logo from '../../assets/test1.png';
+import image from '../../assets/image1.jpeg';
 
 // export default ProfileCard;
 // interface ProfileCardProps {
@@ -15,30 +14,38 @@ import logo from '../../assets/test1.png';
 //   onSettingsClick?: () => void;
 // }
 
-const UserProfileCard: React.FC = () => {
+interface UserProfileCardProps {
+  children?: ReactNode;
+}
+
+const UserProfileCard: React.FC<UserProfileCardProps> = ({ children }) => {
   return (
-    <div className="relative flex h-[256px] w-[1196px] flex-col justify-end border border-transparent bg-transparent">
-      <ProfilePicture
-        src={logo}
-        className="absolute left-[-54px] top-[-54px]"
+    <div className="flex flex-col items-center justify-center">
+      <img
+        src={image}
+        alt="image1.png"
+        className="h-[330px] w-[1920px] object-cover opacity-60"
       />
-      <div className="mt-6 flex items-center justify-between">
-        <div className="flex items-center">
-          <p className="ml-[80px] font-noto-sans-kr text-3xl font-normal leading-normal text-black">
-            정센트 반 희호
-          </p>
-          <p className="font-noto-sans-kr text-sm font-normal leading-normal text-black">
-            &nbsp;(Jeongcent Van Heeho)
-          </p>
+      <div className="relative flex h-[256px] w-[1196px] flex-col justify-end">
+        <ProfilePicture
+          src={logo}
+          className="absolute left-[-54px] top-[-54px]"
+        />
+        <div className="mt-6 flex items-center justify-between">
+          <div className="flex items-center">
+            <p className="ml-[80px] font-noto-sans-kr text-3xl font-normal leading-normal text-black">
+              정센트 반 희호
+            </p>
+            <p className="font-noto-sans-kr text-sm font-normal leading-normal text-black">
+              &nbsp;(Jeongcent Van Heeho)
+            </p>
+          </div>
+          <div className="flex gap-x-4">{children}</div>
         </div>
-        <div className="flex gap-x-4">
-          <LikesButton />
-          <SettingsButton />
+        <ProfileLinks />
+        <div className="flex flex-1 items-center justify-center">
+          <ProfileTextArea />
         </div>
-      </div>
-      <ProfileLinks />
-      <div className="flex flex-1 items-center justify-center">
-        <ProfileTextArea />
       </div>
     </div>
   );
