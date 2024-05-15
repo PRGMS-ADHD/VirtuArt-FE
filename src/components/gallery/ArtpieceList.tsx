cffa2478472eb37197d1e8ea8ae68c854eed185d
kajing1077 <kajing1077@gmail.com>
2024-05-15 18:56:48 +0900

    #31 refactor: Remove commented out code in ArtpieceList component

--- src/components/gallery/ArtpieceList.tsx
+++ src/components/gallery/ArtpieceList.tsx
@@ -6,13 +6,13 @@ const ArtpieceList = () => {
     <div>
       {ArtPieceCategory.map((category) => (
         <div key={category.id}>
-          <div
-            className="mx-2  mt-10 text-sm"
-            style={{ fontFamily: 'noto-sans KR' }}
-          >
-            {category.name}
-          </div>
-          <Artpiece category={category.name} />
+          {/*<div*/}
+          {/*  className="mx-2 mt-10 text-sm"*/}
+          {/*  style={{ fontFamily: 'noto-sans KR' }}*/}
+          {/*>*/}
+          {/*  {category.name}*/}
+          {/*</div>*/}
+          <Artpiece category={category} />
         </div>
       ))}
     </div>
