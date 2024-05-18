6b452ee0fd0bbc5f67541a2742253e9fc678c1fc
희호 <heraok0153@gmail.com>
2024-05-18 21:46:40 +0900

    #14 Refactor
    반응형 수정

--- src/components/artists/ArtistsList.tsx
+++ src/components/artists/ArtistsList.tsx
@@ -1,26 +1,20 @@
 import ArtistsInfo from './ArtistsInfo';
 
-const categoryNames = {
-  1: 'WESTERN',
-  2: 'KOREAN',
-  3: 'POP ART',
-  4: 'MEDIA ART',
-  5: 'MODERN ART'
-};
-
 const ArtistsList = ({ artists, categories }) => {
   return (
-    <div className="w-screen flex flex-col items-center">
+    <div className="flex w-screen flex-col items-center">
       {categories.map((category, index) => {
         const bgClass = index % 2 === 0 ? 'bg-gray-100' : '';
         return (
-          <div key={category.id} className={`p-1 ${bgClass} w-screen`}>
+          <div key={category.id} className={`p-4 ${bgClass} w-screen`}>
             <div className="container mx-auto px-4">
-              <div className="font-noto-sans-kr font-medium text-[20px] leading-[29px]">
-                {categoryNames[category.id]}
-              </div>
+              <ArtistsInfo
+                artists={artists.filter(
+                  (artist) => artist.category === category.id,
+                )}
+                categoryId={category.id}
+              />
             </div>
-            <ArtistsInfo artists={artists.filter(artist => artist.category === category.id)} />
           </div>
         );
       })}
--- src/components/gallery/ArtpieceList.tsx
+++ src/components/gallery/ArtpieceList.tsx
@@ -4,17 +4,18 @@ import { ArtPieceCategory } from '../../data/artPieceCategories';
 const ArtpieceList = () => {
   return (
     <div>
-      {ArtPieceCategory.map((category) => (
-        <div key={category.id}>
-          <div
-            className="mx-2  mt-10 text-sm"
-            style={{ fontFamily: 'noto-sans KR' }}
-          >
-            {category.name}
+      {ArtPieceCategory.map((category, index) => {
+        const isEven = index % 2 === 0;
+        const bgClass = isEven ? 'bg-gray-100' : '';
+        return (
+          <div key={category.id} className={`p-4 ${bgClass}`}>
+            <div className="mx-2 mt-10 text-sm font-noto-sans">
+              {category.name}
+            </div>
+            <Artpiece category={category.name} />
           </div>
-          <Artpiece category={category.name} />
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
