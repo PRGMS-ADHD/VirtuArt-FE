2f4f3be53dbfe41aa2ab5ad5c29b6946a4429b07
희호 <heraok0153@gmail.com>
2024-05-18 21:53:42 +0900

    fix : Artpiecelist page

--- src/components/gallery/ArtpieceList.tsx
+++ src/components/gallery/ArtpieceList.tsx
@@ -4,18 +4,11 @@ import { ArtPieceCategory } from '../../data/artPieceCategories';
 const ArtpieceList = () => {
   return (
     <div>
-      {ArtPieceCategory.map((category, index) => {
-        const isEven = index % 2 === 0;
-        const bgClass = isEven ? 'bg-gray-100' : '';
-        return (
-          <div key={category.id} className={`p-4 ${bgClass}`}>
-            <div className="mx-2 mt-10 text-sm font-noto-sans">
-              {category.name}
-            </div>
-            <Artpiece category={category.name} />
-          </div>
-        );
-      })}
+      {ArtPieceCategory.map((category) => (
+        <div key={category.id}>
+          <Artpiece category={category} />
+        </div>
+      ))}
     </div>
   );
 };
