089c27bd22b5c9bfdca0dd9e6c66e4d4790f7cbc
희호 <heraok0153@gmail.com>
2024-05-21 00:31:18 +0900

    #60 Feat: Add navigation buttons to artist carousel

--- src/components/artists/ArtistsInfo.tsx
+++ src/components/artists/ArtistsInfo.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { MdNavigateNext } from 'react-icons/md';
+import { MdNavigateNext, MdNavigateBefore } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import { ArtistModel } from '@/models/artist.model';
 
 const categoryNames: Record<number, string> = {
-  1: 'WESTERN',
-  2: 'KOREAN',
-  3: 'POP ART',
-  4: 'MEDIA ART',
-  5: 'MODERN ART',
+  1: '서양화',
+  2: '동양화',
+  3: '팝 아트',
+  4: '미디어 아트',
+  5: '현대 미술',
 };
 
 interface ArtistsInfoProps {
@@ -25,8 +25,23 @@ const ArtistsInfo: React.FC<ArtistsInfoProps> = ({ artists, categoryId }) => {
     );
   };
 
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex > 0
+        ? prevIndex - 6
+        : artists.length - (artists.length % 6 || 6),
+    );
+  };
+
   return (
     <div className="relative flex w-full items-center justify-center px-14 py-5">
+      <button
+        onClick={handlePrevious}
+        className="absolute left-[38px] top-1/2 z-10 flex h-[40px] w-[40px] -translate-y-1/2 transform items-center justify-center rounded-full bg-white shadow-md transition-transform duration-500 ease-in-out hover:scale-110"
+        type="button"
+      >
+        <MdNavigateBefore className="text-lg" />
+      </button>
       <div className="relative grid grid-cols-2 gap-1.5 sm:grid-cols-3 md:grid-cols-3 md:grid-rows-1 md:flex-row lg:grid-cols-6">
         <div className="col-span-full font-noto-sans-kr text-[20px] font-medium leading-[29px]">
           {categoryNames[categoryId]}
@@ -63,7 +78,7 @@ const ArtistsInfo: React.FC<ArtistsInfoProps> = ({ artists, categoryId }) => {
       </div>
       <button
         onClick={handleNext}
-        className="absolute right-[38px] flex h-[40px] w-[40px] items-center justify-center rounded-full bg-white shadow-md transition-transform duration-500 ease-in-out hover:scale-110"
+        className="absolute right-[38px] top-1/2 flex h-[40px] w-[40px] -translate-y-1/2 transform items-center justify-center rounded-full bg-white shadow-md transition-transform duration-500 ease-in-out hover:scale-110"
         type="button"
       >
         <MdNavigateNext className="text-lg" />
