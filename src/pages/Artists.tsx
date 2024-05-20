6eb834a48651d42cd2fabd97ede9ed6192b98bdc
희호 <heraok0153@gmail.com>
2024-05-20 15:14:12 +0900

    #50 Refactor: centralize API request logic in http.ts

--- src/pages/Artists.tsx
+++ src/pages/Artists.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import axios from 'axios';
+import { fetchArtists } from '../api/http'; // 수정된 부분
 import ArtistsList from '../components/artists/ArtistsList';
 
 // Artist와 Category 타입 정의
@@ -20,15 +20,13 @@ const Artists = () => {
   const [categories, setCategories] = useState<Category[]>([]);
 
   useEffect(() => {
-    const fetchArtists = async () => {
+    const fetchArtistsData = async () => {
       try {
-        const response = await axios.get<Artist[]>(
-          'http://localhost:3000/artist/all',
-        );
-        setArtists(response.data);
+        const artistsData = await fetchArtists();
+        setArtists(artistsData);
 
         const uniqueCategories = Array.from(
-          new Set(response.data.map((artist) => artist.category)),
+          new Set(artistsData.map((artist) => artist.category)),
         );
         setCategories(
           uniqueCategories.map((category) => ({
@@ -40,7 +38,7 @@ const Artists = () => {
         console.error('Error fetching artists:', error);
       }
     };
-    fetchArtists();
+    fetchArtistsData();
   }, []);
 
   return (
