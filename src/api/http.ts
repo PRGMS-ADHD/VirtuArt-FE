503f4657215832b61d45622cf702505b1797dc44
Munseok Song <song3392@gmail.com>
2024-05-21 17:58:49 +0900

    Update: BASEURL to .env variables

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { getToken, useAuthStore } from '../store/authStore';
 
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 const DEFAULT_TIMEOUT = 30000;
 
 export const createClient = (config?: AxiosRequestConfig) => {
