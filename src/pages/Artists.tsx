83b60dbb8db9dde9c95f483c163cb58e45f19b1b
희호 <heraok0153@gmail.com>
2024-05-21 18:58:07 +0900

    #68 Feat: Adjust layout styles in Artists components

--- src/pages/Artists.tsx
+++ src/pages/Artists.tsx
@@ -31,15 +31,14 @@ const Artists = () => {
   }, []);
 
   return (
-    <div className="my-8 flex flex-col items-center">
+    <div className="my-6 flex flex-col items-center">
       <motion.h1
         initial={{ opacity: 0, x: -100 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1.5 }}
-        className="my-2 font-medium"
+        className="font my-2 text-2xl font-medium"
         style={{
           fontFamily: 'Helvetica Neue',
-          fontSize: 'clamp(24px, 5vw, 20px)',
         }}
       >
         Meet the Artists Behind the Canvas!
@@ -48,10 +47,9 @@ const Artists = () => {
         initial={{ opacity: 0, x: -100 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 2, delay: 0.4 }}
-        className="mb-8 font-extralight"
+        className="mb-6 text-sm font-extralight"
         style={{
           fontFamily: 'Noto Sans KR',
-          fontSize: 'clamp(12px, 5vw, 13px)',
         }}
       >
         창작의 매력에 빠져보세요! 여러분을 기다리는 아티스트들이 있습니다.
